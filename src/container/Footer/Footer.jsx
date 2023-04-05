c730b8a06e41a95c1fc262f9d7f7ef69af5d27ea
kunal-gulati-dev <knlgulati365@gmail.com>
2023-04-05 15:43:06 +0530

    updated the html

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -44,6 +44,7 @@ const Footer = () => (
     <div className="footer__copyright">
       <p className="p__opensans">2021 Gerícht. All Rights reserved.</p>
     </div>
+    <div></div>
   </div>
 );
 
